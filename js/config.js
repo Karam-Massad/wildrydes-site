bce182852ddd5dad82d4d7616ec8e3c2463d4344
Karam-Massad <56916782+Karam-Massad@users.noreply.github.com>
2022-01-29 05:08:39 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-1
     },
     api: {
-        invokeUrl: 'https://igdqjc45uk.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://igdqjc45uk.execute-api.us-east-1.amazonaws.com/prod',
+        invokeUrl: 'https://c12q5amiqe.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://c12q5amiqe.execute-api.us-east-1.amazonaws.com/prod',
     }
 };
